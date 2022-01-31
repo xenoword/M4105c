d300f6419c364429068d73a53b8a278c109ef901
parafeu <muhl.alexandre74@gmail.com>
2022-01-31 19:18:32 +0000

    Initial Commit

---
+++ resources/views/app.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <style>
+        body {
+            font-family: 'Nunito', sans-serif;
+        }
+    </style>
+    <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
+    <script src="{{ mix('/js/app.js') }}" defer></script>
+    @inertiaHead
+</head>
+
+<body class="antialiased">
+    @inertia
+</body>
+
+</html>
---
+++ webpack.mix.js
@@ -0,0 +1,32 @@
+const mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel applications. By default, we are compiling the CSS
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.js('resources/js/app.js', 'public/js')
+    .vue({
+        version: 2
+    })
+    .postCss('resources/css/app.css', 'public/css', [
+        //
+    ])
+    .options({
+        hmrOptions: {
+            host: '51.83.36.122',
+            port: '8080'
+        }
+    })
+    .webpackConfig({
+        devServer: {
+            host: '51.83.36.122',
+            port: '8080'
+        }
+    });
